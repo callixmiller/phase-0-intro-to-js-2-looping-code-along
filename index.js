c4877ed4542914e90a785c7fd2e5750371438cb5
callixmiller <calmill413@gmail.com>
2022-12-13 11:27:30 -0500

    commit tree d69a05bf211d3e6bf4513f3cb332e67b3a713dfd

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let message = [];
+    for (let i = 0; i < names.length; i++) {
+        message[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    };
+    return message;
+}
+writeCards;
+console.log(message);
+
+function countDown() {
+    let countDown = 10;
+        while (countDown >= 0) {
+            console.log(countDown--);
+        }
+}
